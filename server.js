5374ae54d0018de01676580f69705715db0f70cb
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-02 13:51:43 +0100

    Added connection to mongodb database hosted on mlab

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { MONGO_KEY } = require('./keys');
+
 const corsHeaders = require('./middleware/corsHeaders');
 const userRoutes = require('./routes/userRoutes');
-
-const app = express();
 const port = process.env.PORT || 2000;
 
+mongoose.connect(`mongodb://${MONGO_KEY.username}:${MONGO_KEY.password}@ds135776.mlab.com:35776/authenticate`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(corsHeaders);

755f9bd5a3a0085281001f5fd538737eff88ba97
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-06 00:26:11 +0100

    Added socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import socketIOClient from 'socket.io-client';
+import axios from 'axios';
 import './App.scss';
 
 import Home from './pages/Home/Home';
@@ -10,10 +12,35 @@ import Game from './pages/Game/Game';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
+const AuthRoute = ({ auth, render, ...rest }) => <Route {...rest} render={(auth) ? render : () => <Redirect to="/auth" />} />
+const NoAuthRoute = ({ auth, render, ...rest }) => <Route {...rest} render={(!auth) ? render : () => <Redirect to="/dash" />} />
+
 const App = () => {
 	const [user, setUser] = useState(null);
-	const AuthRoute = ({ render, ...rest }) => <Route {...rest} render={(user) ? render : () => <Redirect to="/auth" />} />
-	const NoAuthRoute = ({ render, ...rest }) => <Route {...rest} render={(!user) ? render : () => <Redirect to="/dash" />} />
+	const [socket, setSocket] = useState(null);
+	useEffect(() => {
+		if (socket) {
+			socket.on('connect', () => {
+				console.log("Connected to Socket IO");
+			});
+			
+			socket.on('disconnect', () => {
+				console.log("Disconnected from Socket IO");
+			});
+		}
+	}, [socket])
+
+	const signIn = user => {
+		setUser(user);
+		setSocket(socketIOClient('http://localhost:2000'));
+	};
+
+	const signOut = () => {
+		axios.post('http://localhost:2000/auth/signout');
+		socket.disconnect();
+		setUser(null);
+		setSocket(null);
+	};
 
   return (
     <div className="App">
@@ -21,9 +48,9 @@ const App = () => {
 			<Router>
 				<Switch>
 					<Route exact path='/' component={Home} />
-					<NoAuthRoute exact path='/auth' render={() => <Auth setUser={setUser} />} />
-					<AuthRoute path='/dash' render={() => <Dash user={user} setUser={setUser} />} />
-					<AuthRoute path='/game' render={() => <Game user={user} />} />
+					<NoAuthRoute exact path='/auth' auth={!!user} render={() => <Auth signIn={signIn} />} />
+					<AuthRoute path='/dash' auth={!!user} render={() => <Dash signOut={signOut} user={user} />} />
+					<AuthRoute path='/game' auth={!!user} render={() => <Game user={user} />} />
 				</Switch>
 			</Router>
 			<Footer />
@@ -32,5 +59,3 @@ const App = () => {
 };
 
 export default App;
-
-// {(user) ? <Redirect from='*' to='/dash' /> : <Redirect from='*' to='/' /> }

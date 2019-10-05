375a186c1425abfe9bcddd6cafa45fc9732282ac
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-06 00:21:16 +0100

    Added fields to user model

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,10 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	characters: { type: Array, default: [] },
+	verified: { type: Boolean, default: false },
+	admin: { type: Number, default: 0 },
+	startDate: { type: Date, default: Date.now },
+	lastLogin: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('User', userSchema, 'users');

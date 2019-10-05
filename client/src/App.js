44654b742945d2f808035a71c1a4b89c7e1508d0
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-05 16:52:19 +0100

    Added react router and store session info not jwt

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './App.scss';
 
-import Header from './Header/Header';
-import Auth from './Auth/Auth';
-import Main from './Main/Main';
-import Footer from './Footer/Footer';
+import Home from './pages/Home/Home';
+import Auth from './pages/Auth/Auth';
+import Dash from './pages/Dash/Dash';
+import Game from './pages/Game/Game';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
-	const [token, setToken] = useState(null);
-
-	const renderMain = () => {
-		if (token) return <Main token={token} />
-		else return <Auth setToken={setToken} />
-	};
+	const [user, setUser] = useState(null);
+	const AuthRoute = ({ render, ...rest }) => <Route {...rest} render={(user) ? render : () => <Redirect to="/auth" />} />
+	const NoAuthRoute = ({ render, ...rest }) => <Route {...rest} render={(!user) ? render : () => <Redirect to="/dash" />} />
 
   return (
     <div className="App">
 			<Header title="User Authentication" subtitle="Testing user authentication with React" />
-			{renderMain()}
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<NoAuthRoute exact path='/auth' render={() => <Auth setUser={setUser} />} />
+					<AuthRoute path='/dash' render={() => <Dash user={user} setUser={setUser} />} />
+					<AuthRoute path='/game' render={() => <Game user={user} />} />
+				</Switch>
+			</Router>
 			<Footer />
     </div>
   );
 };
 
 export default App;
+
+// {(user) ? <Redirect from='*' to='/dash' /> : <Redirect from='*' to='/' /> }

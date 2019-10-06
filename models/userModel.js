3b62c34d6f805dc66f16e23db2b3462b466bf403
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-07 00:05:40 +0100

    Added validation requirements

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
+	username: { type: String, required: true, unique: true, minlength: 3, maxlength: 25 },
 	password: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	verified: { type: Boolean, default: false },
-	admin: { type: Number, default: 0 },
-	startDate: { type: Date, default: Date.now },
-	lastLogin: { type: Date, default: Date.now },
+	admin: { type: Number, default: 0, min: 0, max: 5 },
+	createDate: { type: Date, default: Date.now },
+	lastLoginDate: { type: Date, default: null },
 });
 
 module.exports = mongoose.model('User', userSchema, 'users');

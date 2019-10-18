3323048c1cebfad330d8835ceb41696376b9e60d
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-18 19:38:00 +0100

    Added shared session socket connection

--- server.js
+++ server.js
@@ -8,21 +8,20 @@ const bodyParser = require('body-parser');
 const expressSession = require('express-session');
 const sharedSession = require('express-socket.io-session');
 const connectSession = require('connect-mongodb-session');
-
 const passport = require('passport');
 const localStrategy = require('./middleware/passportStrategy');
+
 const authRouter = require('./routes/authRouter');
 const socketHandler = require('./handlers/socketHandler');
-
+const User = require('./models/userModel');
 const { MONGO, SESSION_SECRET } = require('./keys');
 const PORT = process.env.PORT || 2000;
 const MONGO_URI = process.env.MONGO_URI || `mongodb://${MONGO.username}:${MONGO.password}@ds135776.mlab.com:35776/authenticate`;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => console.log("Database connected..."));
 
 const app = express();
 const server = http.Server(app);
-const io = socketIO(server);
 
 const MongoDBStore = connectSession(expressSession);
 const store = new MongoDBStore({
@@ -36,23 +35,28 @@ const session = expressSession({
 	name: 'sessionId',
 	secret: SESSION_SECRET,
 	store: store,
-	resave: true,
-	saveUninitialized: true,
-	cookie: {maxAge: 1000 * 60 * 60},
+	resave: false,
+	saveUninitialized: false,
+	cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}, // 1 year
 });
 
-app.use(cors({ origin: "*"}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session);
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(session);
-io.use(sharedSession(session));
+passport.use(localStrategy);
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser((id, done) => User.findById(id, '_id username email verified createDate lastLoginDate', (err, user) => done(err, user)));
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(localStrategy);
 
 app.use('/auth', authRouter);
+
+const io = socketIO(server, {cookie: false});
+io.use((socket, next) => session(socket.request, {}, next));
+io.use(sharedSession(session, {autoSave: true}));
 io.on('connection', socket => socketHandler(socket));
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));

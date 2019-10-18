4f91277443dacabaa072a02a6cdfcc5af993406c
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-18 19:45:16 +0100

    Added cookieSignIn route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const authHandler = require('../handlers/authHandler');
-const checkAuth = require('../middleware/checkAuth');
-const checkNoAuth = require('../middleware/checkNoAuth');
+const { checkAuth, checkNoAuth } = require('../middleware/checkAuth');
 
 const authRouter = express.Router();
 
+authRouter.get('/', checkAuth, authHandler.cookieSignIn);
 authRouter.post('/signup', checkNoAuth, authHandler.signUp);
 authRouter.post('/signin', checkNoAuth, authHandler.signIn);
 authRouter.post('/signout', checkAuth, authHandler.signOut);

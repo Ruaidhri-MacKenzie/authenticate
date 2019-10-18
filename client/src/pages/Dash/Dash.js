7be9b755672b34a27f69dc0881ca6d7f1c0c4a3e
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-18 19:41:37 +0100

    Removed Routes, added state for main dash display

--- client/src/pages/Dash/Dash.js
+++ client/src/pages/Dash/Dash.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { Route, Link, NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
 import './Dash.scss';
 
 import Characters from '../../components/Characters/Characters';
 import Options from '../../components/Options/Options';
 import CreateCharacter from '../../components/CreateCharacter/CreateCharacter';
 
-const Dash = ({ signOut, user }) => {
+const Dash = ({ signOut, user, logIn }) => {
+	const [state, setState] = useState("characters");
+	const handleClick = e => setState(e.target.name);
+
+	const renderMain = () => {
+		if (state === "characters") {
+			return <Characters characters={user.characters} handleClick={handleClick} logIn={logIn} />
+		}
+		else if (state === "options") {
+			return <Options />
+		}
+		else if (state === "create") {
+			return <CreateCharacter />
+		}
+	};
+
 	return (
 		<main className="dash">
 			<section className="dash-menu">
@@ -14,19 +28,13 @@ const Dash = ({ signOut, user }) => {
 				<h3 className="dash-menu__username">{user.username}</h3>
 
 				<div className="dash-menu__display">
-					<Route exact path="/dash" render={() => <Characters characters={user.characters} />} />
-					<Route exact path="/dash/options" render={() => <Options />} />
-					<Route exact path="/dash/character" render={() => <CreateCharacter />} />
+					{renderMain()}
 				</div>
 
 				<div className="dash-menu__btns">
-					<NavLink exact className="dash-menu__link" activeClassName="dash-menu__link--active" to={'/dash'}>
-						Characters
-					</NavLink>
-					<NavLink exact className="dash-menu__link" activeClassName="dash-menu__link--active" to={'/dash/options'}>
-						Options
-					</NavLink>
-					<Link className="dash-menu__link" to={'/'} onClick={signOut}>Sign Out</Link>
+					<button name="characters" className="dash-menu__link dash-menu__link--active" onClick={handleClick}>Characters</button>
+					<button name="options" className="dash-menu__link" onClick={handleClick}>Options</button>
+					<button className="dash-menu__link" onClick={signOut}>Sign Out</button>
 				</div>
 			</section>
 		</main>

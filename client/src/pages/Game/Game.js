9bb5ee6c3e07e2ec4602f34264836f0eba1b7c2b
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-18 19:42:14 +0100

    Removed link, added logOut callback

--- client/src/pages/Game/Game.js
+++ client/src/pages/Game/Game.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Game.scss';
 
-const Game = ({ user }) => {
+const Game = ({ user, logOut }) => {
 	return (
 		<main className="game">
 			<h1>GAME</h1>
 			<h3>{user.username}</h3>
-			<Link to={'/dash'}>Dashboard</Link>
+			<button onClick={logOut}>Dashboard</button>
 			<canvas className="game-window"></canvas>
 		</main>
 	);

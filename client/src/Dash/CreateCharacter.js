0e5e9985a1c817172cb335fe37ca100bae76c1eb
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-23 17:17:55 +0100

    Added a display of the roles to choose from

--- client/src/Dash/CreateCharacter.js
+++ client/src/Dash/CreateCharacter.js
@@ -1,13 +1,58 @@
 import React, { useState } from 'react';
+import Loading from '../components/Loading/Loading';
+import SpritePreview from '../components/SpritePreview/SpritePreview';
 
-const CreateCharacter = ({ createCharacter }) => {
+import useGlobalState from '../App/state';
+
+const Role = ({ role, handleClick }) => {
+	const { _id, sprite, name } = role;
+	return (
+		<div className="role" data-id={_id} onClick={handleClick}>
+			<SpritePreview className="role__sprite" sprite={sprite} />
+			<p className="role__name">{name}</p>
+		</div>
+	);
+};
+
+const RoleList = ({ roles, loadingDash, handleClick }) => {
+	const renderRoles = () => {
+		if (loadingDash) return <Loading className="role-list__loading" />
+		return (
+			<>
+				{roles && roles.map(role => <Role key={role.name} role={role} handleClick={handleClick} />)}
+			</>
+		);
+	};
+
+	return (
+		<div className="role-list">
+			{renderRoles()}
+		</div>
+	);
+};
+
+const CreateCharacter = () => {
+	const [{ socket, roles, loadingDash }] = useGlobalState();
+
 	const [error, setError] = useState(null);
 	const [inputs, setInputs] = useState({ name: "", role: "" });
-	
+
 	const handleChange = e => setInputs({ ...inputs, [e.target.name]: e.target.value });
+	const handleClick = e => {
+		if (e.currentTarget.classList.contains("role--active")) {
+			e.currentTarget.classList.remove("role--active");
+			setInputs({ ...inputs, role: "" });
+			return;
+		}
+
+		const activeRole = document.querySelector(".role--active");
+		if (activeRole) activeRole.classList.remove("role--active");
+		if (e.currentTarget.classList.contains("role")) e.currentTarget.classList.add("role--active");
+		setInputs({ ...inputs, role: e.currentTarget.dataset.id });
+	};
 	const handleSubmit = e => {
 		e.preventDefault();
-		if (verifyInputs(inputs)) createCharacter(inputs);
+		if (verifyInputs(inputs)) socket.emit('createCharacter', {name: inputs.name.trim(), role: inputs.role});
 	};
 	
 	const verifyInputs = ({ name, role }) => {
@@ -36,9 +81,9 @@ const CreateCharacter = ({ createCharacter }) => {
 	return (
 		<form className="create-char" onSubmit={handleSubmit}>
 			<input className="create-char__input" type="text" name="name" placeholder="Name" value={inputs.name} onChange={handleChange} />
-			<input className="create-char__input" type="text" name="role" placeholder="Role" value={inputs.role} onChange={handleChange} />
+			<RoleList roles={roles} loadingDash={loadingDash} handleClick={handleClick} />
 			{error && <p className="create-char__error">{error}</p>}
-			<button className="create-char__submit">Submit</button>
+			<button className="create-char__submit">Create Character</button>
 		</form>
 	);
 };

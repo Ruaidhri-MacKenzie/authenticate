5778d319ee41c88d2e42b87840aa3d20ad755dea
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-23 17:14:51 +0100

    Populate role when reading character

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -4,12 +4,12 @@ const characterSelect = '_id name role level sprite map x y direction user creat
 const characterInfoSelect = '_id name role level sprite user';
 
 const characterController = {};
-characterController.create = async data => await Character.create({ ...data });
-characterController.readAll = async () => await Character.find().select(characterSelect).exec();
-characterController.read = async id => await Character.findById(id).select(characterSelect).exec();
+characterController.create = async data => await Character.create(data);
+characterController.readAll = async () => await Character.find().select(characterSelect).populate("role").exec();
+characterController.read = async id => await Character.findById(id).select(characterSelect).populate('role').exec();
 characterController.update = async data => await Character.findOneAndUpdate({ _id: data._id}, {$set: data}).select(characterSelect).exec();
 characterController.delete = async id => await Character.deleteOne({ _id: id }).exec();
-characterController.findByUser = async userId => await Character.find({ user: userId }).select(characterInfoSelect).exec();
+characterController.findByUser = async userId => await Character.find({ user: userId }).select(characterInfoSelect).populate('role').exec();
 characterController.checkExistingName = async name => await Character.findOne({ name: { $regex: new RegExp(name, "i") } }).exec();	// Case insensitive regex
 characterController.updateLastLogin = id => Character.updateOne({ _id: id}, {$set: { lastLoginDate: Date.now()}});
 

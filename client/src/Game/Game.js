7ff7adef70e545e5db7abab3bcc461bc0c8bb1f8
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-26 12:51:46 +0100

    Added components for game sections

--- client/src/Game/Game.js
+++ client/src/Game/Game.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Game.scss';
 
-import useGlobalState from '../App/state';
+import GameMenu from './GameMenu';
+import MapName from './MapName';
+import StatusBox from './StatusBox';
+import Profile from './Profile.js';
+import Stats from './Stats';
+import Inventory from './Inventory';
+import Clans from './Clans';
+import Logout from './Logout';
+import Canvas from './Canvas';
+import Chatbox from './Chatbox';
 
 const Game = () => {
-	const [{ socket, player }] = useGlobalState();
+	const [menuView, setMenuView] = useState('inventory');
+	useEffect(() => {
+		let activeBtn = document.querySelector(".game-menu__btn--active");
+		if (activeBtn) activeBtn.classList.remove("game-menu__btn--active");
+		activeBtn = document.querySelector(".game-menu__" + menuView);
+		if (activeBtn) activeBtn.classList.add("game-menu__btn--active");
+	}, [menuView]);
+
+	const renderMenuView = () => {
+		if (menuView === 'profile') return <Profile />;
+		else if (menuView === 'stats') return <Stats />;
+		else if (menuView === 'inventory') return <Inventory />;
+		else if (menuView === 'clans') return <Clans />;
+		else if (menuView === 'logout') return <Logout setMenuView={setMenuView} />;
+	}
 
 	return (
 		<main className="game">
-			<h1>GAME</h1>
-			<h3>{player.name}</h3>
-			<button onClick={e => socket.emit('logOut', player._id)}>Log Out</button>
-			<canvas className="game-window"></canvas>
+			<Canvas />
+			<MapName />
+			<StatusBox />
+			{renderMenuView()}
+			<GameMenu setMenuView={setMenuView} />
+			<Chatbox />
 		</main>
 	);
 };
